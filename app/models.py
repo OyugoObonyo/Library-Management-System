c2c1d1d6968d9b7716c7e007156a7772e0c0c1b8
OyugoObonyo <nyikal1999@gmail.com>
2022-01-30 11:17:11 +0300

    Add association table

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+# An association table with a record of students who've borrowed a book(s)
+user_book = db.Table('user_book',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'))
+)
+
+
 class User(UserMixin, db.Model):
     """
     A class that represents the user table in the database
@@ -14,6 +21,8 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(32), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    # Attach a record of books borrowed by a user
+    borrowed_books = db.relationship('Book', secondary=user_book, backref="borrowers")
 
     def set_password(self, password):
         """
@@ -43,3 +52,22 @@ def load_user(id):
     returns the user id
     """
     return User.query.get(int(id))
+
+
+class Book(db.Model):
+    """
+    A class that represents book table in the database
+    """
+    id = db.Column(db.Integer, index=True, primary_key=True)
+    synopsis = db.Column(db.String(500), nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+    author = db.Column(db.String(50))
+    year_of_publish = db.Column(db.Integer)
+    category = db.Column(db.String(50))
+    img_url = db.Column(db.String(40))
+
+    def __repr__(self):
+        """
+        Returns a string representation of a book object
+        """
+        return f"<Book_id: {self.id}, Book_title: {self.title} by {self.author}>"
