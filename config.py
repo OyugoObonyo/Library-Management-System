471fa6bf820f922d8f8b81e1953113ebb5a3cdc8
OyugoObonyo <nyikal1999@gmail.com>
2022-01-31 09:32:27 +0300

    Update configuartion file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ A module that contains the application's configuration variables
 import os
 
 # get the app's root directory
-root_dir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
@@ -13,7 +13,8 @@ class Config(object):
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key-for-development'
     # Add sqlite as development db
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(root_dir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOK_IMAGES_DIR = 'static/images/books'
     # Limit maximum length of book cover image to 5MB
