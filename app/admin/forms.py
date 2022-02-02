35659d437065e5560911c8bb908107635f01cc8e
OyugoObonyo <nyikal1999@gmail.com>
2022-02-02 15:46:28 +0300

    complete ckeditor integration

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 from flask_wtf.file import FileField, FileAllowed
+from flask_ckeditor import CKEditorField
 
 
 class AddBookForm(FlaskForm):
@@ -12,7 +13,7 @@ class AddBookForm(FlaskForm):
     A class that creates a form to add books to the database
     """
     title = StringField('Book title', validators=[DataRequired()])
-    synopsis = StringField('Book description', validators=[DataRequired()])
+    synopsis = CKEditorField('Book description', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     # Only accept numbers greater than 0
     year_of_publish = IntegerField('Year of publishment', validators=[DataRequired(), NumberRange(min=0)])
@@ -25,6 +26,6 @@ class UpdateBookForm(FlaskForm):
     A class that creates a form to update a particular book in the database
     """
     title = StringField('Book title', validators=[DataRequired()])
-    synopsis = StringField('Book description', validators=[DataRequired()])
+    synopsis = CKEditorField('Book description', validators=[DataRequired()])
     category = StringField('Category', validators=[DataRequired()])
     submit = SubmitField('Update book')
