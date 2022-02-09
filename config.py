de5b60fa5c4b2bffd6558ebd9ce396b57c20c4ed
OyugoObonyo <nyikal1999@gmail.com>
2022-02-09 14:46:39 +0300

    Update app configuration file

--- config.py
+++ config.py
@@ -13,11 +13,12 @@ class Config(object):
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key-for-development'
     # Add sqlite as development db
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOK_IMAGES_DIR = 'static/images/books'
     # Limit maximum length of book cover image to 5MB
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+    WHOOSH_BASE = 'whoosh'
 
 # os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
