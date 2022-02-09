b972f9e4e553caf46620137324d0fd3e50a4b101
OyugoObonyo <nyikal1999@gmail.com>
2022-02-09 14:52:50 +0300

    create database models

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from app import create_app
+from app import create_app, db
 
 # Create an app instance
 app = create_app()
+
+with app.app_context():
+    if db is None:
+        db.create_all()
+
 
 if __name__ == ("__main__"):
     app.run(debug=True)
