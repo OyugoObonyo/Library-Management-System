05bc49922d3c0d328e394a1534c0bcae2d472a3e
OyugoObonyo <nyikal1999@gmail.com>
2022-02-09 14:59:24 +0300

    FIX: Create app context

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from app import create_app, db
 
 # Create an app instance
 app = create_app()
-
-db.create_all()
-
+with app.app_context():
+    db.create_all()
 
 if __name__ == ("__main__"):
     app.run(debug=True)
