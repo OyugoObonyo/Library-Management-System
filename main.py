4af4a5b56aed7c6937ff18f8f9e0439ef6a3dfd1
OyugoObonyo <nyikal1999@gmail.com>
2022-02-09 14:55:55 +0300

    fix main file

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from app import create_app, db
 # Create an app instance
 app = create_app()
 
-with app.app_context():
-    if db is None:
-        db.create_all()
+db.create_all()
 
 
 if __name__ == ("__main__"):
